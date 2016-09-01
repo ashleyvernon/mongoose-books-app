5d8876454b191fb791acdd48deea34c64f92e4ac
ashleyvernon <avernon526@gmail.com>
2016-08-31 20:23:39 -0700

    sprint two complete without challenge

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 // server.js
 // SERVER-SIDE JAVASCRIPT
 
+
+/////////////////////////////
+//  SETUP and CONFIGURATION
+/////////////////////////////
+
+var db = require('./models')
+
 //require express in our app
 var express = require('express'),
-  bodyParser = require('body-parser'),
-  db = require('./models');
+  bodyParser = require('body-parser');
 
 // generate a new express app and call it 'app'
 var app = express();
@@ -15,6 +21,27 @@ app.use(express.static('public'));
 // body parser config to accept our datatypes
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
+
+////////////////////
+//  DATA
+///////////////////
+
+
+
+
+
+
+
+
+
+////////////////////
+//  ROUTES
+///////////////////
+
+
+
+
 // define a root route: localhost:3000/
 app.get('/', function (req, res) {
   res.sendFile('views/index.html' , { root : __dirname});
@@ -23,44 +50,82 @@ app.get('/', function (req, res) {
 // get all books
 app.get('/api/books', function (req, res) {
   // send all books as JSON response
-  db.Book.find(function(err, books){
-    if (err) { return console.log("index error: " + err); }
-    res.json(books);
-  });
+  db.Book.find()
+    .populate('author')
+    .exec(function(err, books){
+      if (err) { return console.log("index error: " + err); }
+      res.json(books);  
+    });
 });
+
 
 // get one book
 app.get('/api/books/:id', function (req, res) {
   // find one book by its id
-  db.Book.findById(req.params.id, function(err, book){
-    if (err) { return console.log("show error: " + err); }
-    res.json(book);
-  });
+  console.log('books show', req.params);
+  for(var i=0; i < books.length; i++) {
+    if (books[i]._id === req.params.id) {
+      res.json(books[i]);
+      break; // we found the right book, we can stop searching
+    }
+  }
 });
 
 // create new book
 app.post('/api/books', function (req, res) {
   // create new book with form data (`req.body`)
-  var newBook = new db.Book(req.body);
-  // add newBook to database
-  newBook.save(function(err, book){
-    if (err) { return console.log("create error: " + err); }
-    console.log("created ", book.title);
-    res.json(book);
+  var newBook = new db.Book({ 
+    title: req.body.title,
+    image: req.body.image,
+    releaseDate: req.body.releaseDate,
+  });
+
+  // this code will only add an author to a book if the author already exists
+  db.Author.findOne({name: req.body.author}, function(err, author){
+    newBook.author = author;
+    newBook.save(function(err, book){
+      if (err) {
+        return console.log("create error: " + err);
+      }
+      console.log("created ", book.title);
+      res.json(Book);
+    });
   });
 });
 
+// update book
+app.put('/api/books/:id', function(req,res){
+// get book id from url params (`req.params`)
+  console.log('books update', req.params);
+  var bookId = req.params.id;
+  // find the index of the book we want to remove
+  var updateBookIndex = books.findIndex(function(element, index) {
+    return (element._id === parseInt(req.params.id)); //params are strings
+  });
+  console.log('updating book with index', deleteBookIndex);
+  var bookToUpdate = books[deleteBookIndex];
+  books.splice(updateBookIndex, 1, req.params);
+  res.json(req.params);
+});
 
 // delete book
 app.delete('/api/books/:id', function (req, res) {
   // get book id from url params (`req.params`)
-  console.log(req.params)
+  console.log('books delete', req.params);
   var bookId = req.params.id;
-
-  db.Book.findOneAndRemove({ _id: bookId }, function (err, deletedBook) {
-    res.json(deletedBook);
+  // find the index of the book we want to remove
+  var deleteBookIndex = books.findIndex(function(element, index) {
+    return (element._id === parseInt(req.params.id)); //params are strings
   });
+  console.log('deleting book with index', deleteBookIndex);
+  var bookToDelete = books[deleteBookIndex];
+  books.splice(deleteBookIndex, 1);
+  res.json(bookToDelete);
 });
+
+
+
+
 
 app.listen(process.env.PORT || 3000, function () {
   console.log('Example app listening at http://localhost:3000/');
